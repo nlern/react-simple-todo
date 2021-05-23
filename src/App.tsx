f8713089036e1a549ffb836b42c853b91ee3eee1
Shantanu Dutta <shantanudutta1994@gmail.com>
2021-05-23 09:57:58 +0530

    Implemented display of todos with done state.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import TodoList from './components/TodoList/TodoList';
+import { Todo } from './interfaces';
 
 function App(): JSX.Element {
-  return <></>;
+  const todos: Todo[] = [
+    {
+      id: '1',
+      title: 'Learn React',
+      isDone: true,
+    },
+    {
+      id: '2',
+      title: 'Learn GraphQL',
+      isDone: false,
+    },
+  ];
+  return (
+    <>
+      <TodoList todos={todos} />
+    </>
+  );
 }
 
 export default App;
